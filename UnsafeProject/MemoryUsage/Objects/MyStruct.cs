e3e6050f76bc86a9dd5c1da9316df9177273549b
Oleg Togushakov <oleg.togushakov@eurostudio.net>
2022-05-05 14:05:09 +0700

    Add unsafe project

---
+++ UnsafeProject/MemoryUsage/Objects/MyStruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnsafeProject.MemoryUsage.Objects
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MyStruct
+    {
+        public MyStruct(int n, double r) => (integer, real) = (n, r);
+
+        public int integer;
+
+        public double real;
+    }
+}
---
+++ UnsafeProject/Pointers/PointerOperators.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnsafeProject.Pointers
+{
+    public unsafe class PointerOperators
+    {
+        public static void DiscoverAddAndSubstruct()
+        {
+            const int count = 3;
+            int[] numbers = new int[count] { 10, 20, 30 };
+            fixed (int* pointerToFirst = &numbers[0])
+            {
+                int* pointerToLast = pointerToFirst + (count - 1);
+
+                Console.WriteLine($"Value {*pointerToFirst} at address {(long)pointerToFirst}");
+                Console.WriteLine($"Value {*pointerToLast} at address {(long)pointerToLast}");
+            }
+        }
+
+        public static void DiscoverSubstruct()
+        {
+            int* numbers = stackalloc int[] { 0, 1, 2, 3, 4, 5 };
+
+            int* p1 = &numbers[1];
+            int* p2 = &numbers[5];
+            long result = p2 - p1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
