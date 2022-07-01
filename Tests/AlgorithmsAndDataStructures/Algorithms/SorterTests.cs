1a99bc7e952dc88634d11bb18b18cf4dfbe58624
Oleg Togushakov <oleg.togushakov@eurostudio.net>
2022-07-01 12:16:01 +0700

    Add quick sort v2

--- Tests/AlgorithmsAndDataStructures/Algorithms/SorterTests.cs
+++ Tests/AlgorithmsAndDataStructures/Algorithms/SorterTests.cs
@@ -1,5 +1,7 @@
 using AlgorithmsAndDataStructures.Algorithms.Sorter;
+
 using FluentAssertions;
+
 using Xunit.Abstractions;
 
 namespace Tests.AlgorithmsAndDataStructures.Algorithms
@@ -7,6 +9,9 @@ namespace Tests.AlgorithmsAndDataStructures.Algorithms
     public class SorterTests
     {
         private readonly ITestOutputHelper _output;
+
+        private int[] _sourceArray => new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
 
         public SorterTests(ITestOutputHelper output)
         {
@@ -17,11 +22,28 @@ namespace Tests.AlgorithmsAndDataStructures.Algorithms
         public void QuickSortTest()
         {
             // Arrange
-            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int[] arr = _sourceArray;
             
 
             // Act
             Sorter.QuickSort(arr, 0, arr.Length-1);
+
+            foreach (var item in arr)
+                _output.WriteLine(" " + item);
+            
+            // Assert
+            arr.Should().BeInAscendingOrder();
+        }
+
+        [Fact]
+        public void QuickSortV2Test()
+        {
+            // Arrange
+            int[] arr = _sourceArray;
+            
+
+            // Act
+            Sorter.QuickSortV2(arr);
 
             foreach (var item in arr)
                 _output.WriteLine(" " + item);
